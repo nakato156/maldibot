b5b615729e761722d553550be847e1c9942de03f
nakato156 <christianvelasces@gmail.com>
2021-07-04 09:18:12 -0500

    change method for connection to db

--- db/conexion.js
+++ db/conexion.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 const uri = process.env.DB_CONNECTION
-mongoose.createConnection(uri,{ useNewUrlParser: true, useUnifiedTopology: true})
-.on('open', ()=>console.log("Conected"))
-.on('error', err => console.log("error",err))
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(y=>console.log("Conected"))
+.catch(err => console.log("error",err))
